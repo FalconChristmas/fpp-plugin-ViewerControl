c95d8965282dbaadd94f4bcb834689ae3754f551
Chris Pinkham <cpinkham@bc2va.org>
2019-01-24 18:18:52 -0800

    Add a simple About page with a few useful links.

---
+++ about.php
@@ -0,0 +1,9 @@
+<center><b>Viewer Control plugin by Chris Pinkham aka CaptainMurdoch</b></center>
+<br>
+<fieldset>
+<legend>Useful Links</legend>
+<a href='https://github.com/FalconChristmas/fpp-plugin-ViewerControl/commits/master' target='_blank'>Commit log</a> (plugin only, not for the main site)<br>
+<a href='https://github.com/FalconChristmas/fpp-plugin-ViewerControl/issues' target='_blank'>Bug/Issue Tracker</a><br>
+<a href='https://falconchristmas.com/forum/index.php/topic,10365.0.html' target='_blank'>Main Plugin/Site Discussion thread on FalconChristmas.com</a><br>
+
+</fieldset>
